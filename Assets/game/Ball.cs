b86b49cb42306d1d3919d3e1fd930010dc8348a4
Thomas Neuraz [HK-ITS] <TNeuraz@kerrylogistics.com>
2016-03-09 15:56:41 +0800

    Initial commit - Pushing project to GitHub Repo

---
+++ Assets/game/Ball.cs
@@ -0,0 +1,105 @@
+﻿/*
+	purpose: class that controls the human player/ball
+	
+	Game Template
+	(c) 2015 Appmobix Ltd
+	support@appmobix.com
+	www.appmobix.com
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Ball : MonoBehaviour
+{
+	Rigidbody2D rb;
+	static Ball pthis;
+	public AudioSource asource;
+	public AudioClip kick, bounce;
+	
+	void playSound(AudioClip c)
+	{
+		asource.clip = c;
+		asource.Play();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (Lib.debug_skipCollisions)
+			return;
+	}
+	
+	public static Ball GetInstance()
+	{
+		return pthis;
+	}
+	
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		pthis = this;
+		
+		rb.AddForce(new Vector2(0f,1000f));
+	}
+	
+	public Vector2 getPosition()
+	{
+		return rb.position;
+	}
+	
+	public Vector2 getVelocity()
+	{
+		return rb.velocity;
+	}
+	
+	public void setPlayerVelocity(float dx, float dy)
+	{
+		rb.velocity = new Vector2(dx,dy);
+	}
+	
+	void FixedUpdate()
+	{
+		float w=SHARED.SW, h=SHARED.SH;
+		
+		Vector2 p = rb.position;
+		Vector2 v = rb.velocity;
+		
+		//elastically bounce off the wall
+		if (p.x<=-w/2f)	{
+			v.x=Mathf.Abs(v.x);
+			playSound(bounce);
+		}
+		if (p.x>=w/2f) {
+			v.x=-Mathf.Abs(v.x);
+			playSound(bounce);
+		}
+		rb.velocity = v;
+		
+		rb.position = p;
+		if (p.y<-h/2f)
+			screen_game.GetInstance().onPlayerDie();
+	}
+	
+	public void kickMe(Vector2 tappos)
+	{
+		Rigidbody2D rb2 = Ball.GetInstance().gameObject.GetComponent<Rigidbody2D>();
+		
+		//ignore if ball travelling upwards
+		if (rb2.velocity.y>=0)
+			return;
+			
+		//tap too far
+		if (Lib.pythag(tappos,rb2.position)>=0.8f)
+			return;
+		
+		float dx = -(tappos.x - rb2.position.x) * 500f;
+		rb2.AddForce(new Vector2(dx,1500f));
+		
+		screen_game game = screen_game.GetInstance();
+		game.doCollision(tappos);
+		game.increaseScore(1);
+		playSound(kick);
+	}
+}
---
+++ Assets/game/screen_game.cs
@@ -0,0 +1,234 @@
+/*
+	purpose: class to manage the game
+	
+	Game Template
+	(c) 2015 Appmobix Ltd
+	support@appmobix.com
+	www.appmobix.com
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class screen_game : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler  {
+	
+	public SpriteRenderer rootsprite;
+	public Text hiscore, score;
+	public Button pausebtn, closebtn;
+	public Image gameover, go, paused;
+	public GameObject ExplosionPrefab;
+	
+	enum STATE {
+		GAMEOVER,
+		PLAYING,
+		PAUSED
+	};
+	STATE state = STATE.PLAYING;
+	bool bIntroIsVisible;
+	
+	bool bHiScore = false;
+	
+	public AudioSource asource;
+	
+	public bool isGameOver()	{return state==STATE.GAMEOVER;}
+	public bool isPaused()		{return state==STATE.PAUSED;}
+	bool isPlaying()		{return state==STATE.PLAYING;}
+	
+	static screen_game pthis;
+	public static screen_game GetInstance() {return pthis;}
+	
+	const float TIME_SHOWING_INTRO = 3f;
+	
+	public GameObject getRootSprite()
+	{
+		return rootsprite.gameObject;
+	}
+	
+	public void increaseScore(int deltaScore)
+	{
+		if (isGameOver())
+			return;
+		SHARED s = SHARED.GetInstance();
+		s.data.score += deltaScore;
+		updateDisplay();
+	}
+	
+	public void doCollision(Vector3 pos)
+	{
+		Object explosion = Instantiate(ExplosionPrefab, pos, Quaternion.identity);
+        StartCoroutine(releaseExplosionGo(explosion));
+        
+		//asource.Play();
+	}
+	
+	IEnumerator releaseExplosionGo(Object explosion)
+	{
+		yield return new WaitForSeconds(1f);
+		Lib.DestroyObject(explosion);
+	}
+	
+	public void onPlayerDie()
+	{
+		SHARED s = SHARED.GetInstance();
+		if (s.data.score > s.data.hiscore) {
+			s.data.hiscore = s.data.score;
+			s.save();
+			bHiScore = true;
+		}
+		
+		if (Lib.debug_invincible)
+			return;
+			
+		//screen_game.GetInstance().onGameOver();
+		Lib.DestroyObject(Ball.GetInstance().gameObject);
+		setState_gameOver();
+		
+		updateDisplay();
+		
+		SimpleAdScript.GetInstance().showInterstitialAd();
+	}
+	
+	Vector3 posstart;
+	float timeGameOver;
+	void setState_gameOver()
+	{
+		timeGameOver = Time.timeSinceLevelLoad;
+		state = STATE.GAMEOVER;
+	}
+	
+	void setState_togglePause()
+	{
+		state = isPaused() ? STATE.PLAYING : STATE.PAUSED;
+	}
+	
+	void updateDisplay()
+	{
+		SHARED s = SHARED.GetInstance();
+		hiscore.text = "Hiscore : " + Lib.ConvNumtoStrThousandSep(s.data.hiscore);
+		score.text = "Score : " + Lib.ConvNumtoStrThousandSep(s.data.score);
+		
+		bool b = Lib.fpart(Time.timeSinceLevelLoad) >= .4f;
+		if (isGameOver())
+			score.gameObject.SetActive(bHiScore ? b : true);
+		pausebtn.gameObject.SetActive(isPlaying() && !bIntroIsVisible);
+		closebtn.gameObject.SetActive(isPaused());
+		gameover.gameObject.SetActive( isGameOver());
+		paused.gameObject.SetActive(b && isPaused());
+		go.gameObject.SetActive(b && isPlaying() && bIntroIsVisible);
+	}
+	
+	public void OnButtonPress_Close()
+	{
+		Lib.LoadScene(SHARED.MENUSCENE);
+	}
+	
+	Vector2[] pausevelocity;
+	public void OnButtonPress_Pause()
+	{
+		Rigidbody2D[] rbarr = UnityEngine.Object.FindObjectsOfType<Rigidbody2D>();
+		int n = rbarr.Length;
+		if (!isPaused())
+			pausevelocity = new Vector2[n];
+		for (int i=0; i<n; i++) {
+			if (isPlaying()) {
+				pausevelocity[i] = rbarr[i].velocity;
+				rbarr[i].velocity = new Vector2(0f,0f);
+//				rbarr[i].Sleep();
+				rbarr[i].constraints = RigidbodyConstraints2D.FreezePosition;
+			}
+			else {
+//				rbarr[i].WakeUp();
+				rbarr[i].constraints = RigidbodyConstraints2D.None;
+				rbarr[i].velocity = pausevelocity[i];
+			}
+		}
+		
+		setState_togglePause();
+		updateDisplay();
+	}
+	
+	void Awake()
+	{
+		pthis = this;
+	}
+	
+	// Use this for initialization
+	void Start ()
+	{
+		MonoObj.Create();
+		
+		Lib.ScaleToScreen(rootsprite);
+		
+		SHARED s = SHARED.GetInstance();
+		s.data.score = 0;
+		
+		bIntroIsVisible = true;
+		updateDisplay();
+		StartCoroutine(co_updateDisplay());
+		//StartCoroutine(co_generateBaddies());
+		//Ship.StartGeneratingShips();
+	}
+	
+	IEnumerator co_updateDisplay()
+	{
+		for (float t=Time.timeSinceLevelLoad; t+TIME_SHOWING_INTRO>Time.timeSinceLevelLoad; ) {
+			yield return new WaitForSeconds(.1f);
+			updateDisplay();
+		}
+		
+		bIntroIsVisible = false;
+		updateDisplay();
+		
+		while (true) {
+			yield return new WaitForSeconds(.1f);
+			updateDisplay();
+		}
+	}
+	
+	void IPointerDownHandler.OnPointerDown(PointerEventData e)
+	{
+		if (isPaused()) {
+			OnButtonPress_Pause();
+			return;
+		}
+		
+		if (isGameOver() && (Time.timeSinceLevelLoad - timeGameOver >= 0.8f) ) {
+			Lib.ReloadScene();
+		}
+		
+		if (isGameOver())
+			return;
+			
+		Vector2 tappos = transScreenPosToRigidBodySpace(e.position);
+		Ball.GetInstance().kickMe(tappos);
+		//doCollision(tappos);
+	}
+	
+	void IPointerUpHandler.OnPointerUp(PointerEventData e)
+	{
+	}
+	
+	void IDragHandler.OnDrag(PointerEventData e)
+	{
+	}
+	
+	void IBeginDragHandler.OnBeginDrag(PointerEventData e)
+	{
+	}
+	
+	void IEndDragHandler.OnEndDrag(PointerEventData e)
+	{
+	}
+	
+	Vector2 transScreenPosToRigidBodySpace(Vector2 pos)
+	{
+		Vector2 v = pos;
+		v.x = Lib.TransformValueToNewRange(0f,v.x,Lib.GetScreenWidth(),
+		                                   -SHARED.SW/2f, SHARED.SW/2f);
+		v.y = Lib.TransformValueToNewRange(0f,v.y,Lib.GetScreenHeight(),
+		                                   -SHARED.SH/2f, SHARED.SH/2f);
+		return v;
+	}
+}
